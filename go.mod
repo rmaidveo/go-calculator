5fe75a74788620835cb9a58a0cdb5026a8c06701
rmaidveo <ilonazelenaya@gmail.com>
2024-02-13 23:07:58 +0300

    Add the Token structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rmaidveo/go-calculator
+
+go 1.20
---
+++ tokenizer/tokenizer.go
@@ -0,0 +1,23 @@
+package tokenizer
+
+type TokenKind int
+
+const (
+	PlusToken TokenKind = iota
+	MinusToken
+	AsteriskToken
+	SlashToken
+	PercentToken
+	ExponentiationToken
+	NumberToken
+	IdentifierToken
+	LeftParenthesisToken
+	RightParenthesisToken
+	CommaToken
+)
+
+type Token struct {
+	Kind     TokenKind
+	Value    string
+	Position int
+}
